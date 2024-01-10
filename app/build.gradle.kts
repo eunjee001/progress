ab85542ddf7e127000f7bf21e29b6f386191605a
eunkyoung99 <eunjee001@gmail.com>
2024-01-10 10:02:17 +0900

    점으로 된 프로그래스 바

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,9 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.github.ybq:Android-SpinKit:1.4.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/kkyoungs/progress/MainActivity.kt
+++ app/src/main/java/com/kkyoungs/progress/MainActivity.kt
@@ -1,37 +1,30 @@
 package com.kkyoungs.progress
 
+import android.R
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.ProgressBar
 import androidx.core.view.isVisible
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.PulseRing
 import com.kkyoungs.progress.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var isStarted = false
-    var progressStatus = 0
-    var handler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val delayMills = 5000L
-//        Handler(Looper.myLooper()!!).postDelayed({
-//            binding.progressCircular.isVisible = false
-//        }, delayMills)
-//        handler = Handler(Handler.Callback {
-//            if (isStarted) {
-//                progressStatus++
-//            }
-//            binding.progressHorizontal.progress = progressStatus
-//            handler?.sendEmptyMessageDelayed(0, 100)
-//
-//            true
-//        })
+//        val circleSprite = CircleSprite()
+//        binding.spinKit.setIndeterminateDrawable(circleSprite)
 
-        handler?.sendEmptyMessage(0)
+
     }
-}
+    }
